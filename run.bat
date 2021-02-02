a679074425ff515670b39a38722254b5a8676f41
MANAS <manasnayak82@gmail.com>
2021-02-02 20:29:44 +0530

    2ndFeb 08:29 p.m.

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-pytest -v -s --alluredir="C:\Users\mnayak\PycharmProjects\OneStopCPQ\allure-results" TestCases
+pytest -v -s --alluredir="C:\Users\mnayak\PycharmProjects\OneStopCPQ\allure-results" TestCases\test_TC_001_TestLogin.py
 rem	pytest -v -s -m "Split" --html=reports\SplitCart.html TestCases
 rem	pytest -v -s -m "TurboPricing" --html=reports\TurboPricing.html TestCases
