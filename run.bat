f100b8504a3c7709401d6cc91b080daaa3220cf7
MANAS <manasnayak82@gmail.com>
2021-02-02 21:42:56 +0530

    2ndFeb 09:42 p.m.

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-pytest -v -s --alluredir="C:\Users\mnayak\PycharmProjects\OneStopCPQ\allure-results" TestCases\test_TC_001_TestLogin.py
+pytest -v -s --alluredir="C:\Users\mnayak\PycharmProjects\OneStopCPQ\allure-results" TestCases
