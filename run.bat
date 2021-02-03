35da006538b329d29c41ac27f62fc96988906563
MANAS <manasnayak82@gmail.com>
2021-02-03 09:01:47 +0530

    3rdFeb 09:01 a.m.

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-pytest -v -s --alluredir="C:\Users\mnayak\PycharmProjects\OneStopCPQ\allure-results" TestCases\test_TC_005_TestMassUpdateO
-nCart.py
+pytest -v -s --alluredir="C:\Users\mnayak\PycharmProjects\OneStopCPQ\allure-results" TestCases\test_TC_005_TestMassUpdateOnCart.py
